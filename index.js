35872b7b1d01500ffd7067aece6997bf079e6ada
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2023-05-25 21:35:00 +0600

    added get fot menu and review

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 
+//MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvsxjlv.mongodb.net/?retryWrites=true&w=majority`;
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.nvsxjlv.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,20 +23,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const menuCollection= client.db('bistroDb').collection('menu')
+    const reviewCollection= client.db('bistroDb').collection('reviews')
 
+    app.get('/menu', async(req, res)=> {
+        const result= await menuCollection.find().toArray()
+        res.send(result)
+    })
 
+    app.get('/reviews', async(req, res)=> {
+        const result= await reviewCollection.find().toArray()
+        res.send(result)
+    })
 
 
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  } 
+  
+  finally {}
 }
 run().catch(console.dir);
 
